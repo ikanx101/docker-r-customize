67df04f04701a26bc3ed15cd902a6fa49ea7049a
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2023-07-25 17:04:02 +0700

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,6 +1,9 @@
+sudo apt-get install ca-certificates curl gnupg -y
+
+
 # Command to run the container
 
-docker run --rm -p 8888:8787 -v /mnt/chromeos/removable/Workstation \
+docker run --rm -p 8888:8787 \
            -e USER=ikanx101 \
            -e PASSWORD=ikanx101 \
            -e USERID=1001 \
