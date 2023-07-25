548c162c62f8010f0149e63c94c3b65071b1e34d
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2023-07-25 17:15:04 +0700

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,8 +1,4 @@
-sudo apt-get install ca-certificates curl gnupg -y
-
-
 # Command to run the container
-
 docker run --rm -p 8888:8787 \
            -e USER=ikanx101 \
            -e PASSWORD=ikanx101 \
