6c7b03e9e07a9bafa1d9b10b180b5ec1c86d2bdd
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2024-09-17 11:54:48 +0700

    Update run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -1,7 +1,15 @@
-# Command to run the container
-docker run --rm -p 8888:8787 \
+# jalan di root tanpa autentifikasi
+docker run --rm -p 8888:8787 -d --name ccc \
            -e USER=ikanx101 \
            -e PASSWORD=ikanx101 \
+           -e DISABLE_AUTH=true \
            -e USERID=1001 \
            -e GROUPID=1001 \
-           ikanx101/r-custom:latest
+           -e ROOT=TRUE \
+           -v /home/ikanx101:/root ikanx101/r-custom:latest
+
+# jalan docker standalone firefox
+docker run -d -p 4445:4444 --name selesele selenium/standalone-firefox:4.8.0-20230123
+
+# cek docker stats
+docker stats
