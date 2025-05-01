060766f607a11f35d8687a28ca4efcfef87434db
mr.ikanx <54991077+ikanx101@users.noreply.github.com>
2025-05-01 20:52:19 +0700

    Create set_awal.sh

---
+++ set_awal.sh
@@ -0,0 +1,17 @@
+# run dalam root
+
+# kita update dan upgrade sistem linux nya
+apt update
+apt upgrade -y
+
+# kita akan install python3 environment terlebih dahulu
+apt install python3-venv
+python3 -m venv .env
+source .env/bin/activate
+
+# install macem-macem
+pip install keras
+pip install tensorflow
+pip install transformers
+pip install 'transformers[torch]'
+pip install diffusers["torch"] transformers
